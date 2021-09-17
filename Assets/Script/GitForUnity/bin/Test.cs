e871a88fef0505877350177966ec1604bf5f0f6d
DearChar <986035796@qq.com>
2021-09-18 00:31:00 +0800

    first cmd

---
+++ Assets/Script/GitForUnity/bin/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using LibGit2Sharp;
+
+public class Test : MonoBehaviour
+{
+    [MenuItem("Git for Unity/Test1")]
+    public static void TestMethod1()
+    {
+        Debug.Log("test");
+    }    
+}
---
+++ Assets/Script/GitForUnity/bin/TestWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestWindow : EditorWindow
+{
+    [MenuItem("Git for Unity/OpenWindows")]
+    public static void CreateTestWindow()
+    {
+        TestWindow tw = GetWindow<TestWindow>();
+    }
+}
