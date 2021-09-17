3e97c4c1c812f8fa98959da311f76769bb1bb55a
DearChar <986035796@qq.com>
2021-09-18 01:56:01 +0800

    good night!

---
+++ Assets/Script/GitForUnity/Editor/GlobalProperties.cs
@@ -0,0 +1,40 @@
+﻿using LibGit2Sharp;
+using System;
+using System.IO;
+
+namespace GitForUnity
+{
+    public class GlobalProperties
+    {
+        public static string ProjectPath
+        {
+            get
+            {
+                return System.Environment.CurrentDirectory;
+            }
+        }
+
+
+        public static bool IsVoidGitPath(string path, ref string voidPath)
+        {
+            voidPath = string.Empty;
+            if (!Directory.Exists(path))
+            {
+                return false;
+            }
+
+            DirectoryInfo dirInfo = Directory.CreateDirectory(path);
+            while (dirInfo != null)
+            {
+                if(Repository.IsValid(dirInfo.FullName))
+                {
+                    voidPath = dirInfo.FullName;
+                    return false;
+                }
+                dirInfo = dirInfo.Parent;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ Assets/Script/GitForUnity/Editor/LogWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using LibGit2Sharp;
+
+namespace GitForUnity
+{
+    public class LogWindow : EditorWindow
+    {
+        [MenuItem("Git for Unity/Log Window")]
+        public static void CreateTestWindow()
+        {
+            LogWindow tw = GetWindow<LogWindow>();
+
+
+
+            //Repository.IsValid()
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label(System.Environment.CurrentDirectory);
+
+            GUILayout.Label(Repository.IsValid(System.Environment.CurrentDirectory).ToString());
+            GUILayout.Label(Repository.IsValid(Application.dataPath).ToString());
+            GUILayout.Label(Repository.IsValid("gdfdgfg").ToString());
+            GUILayout.Label(System.IO.Directory.Exists("gdfdgfg").ToString());
+            
+            //GUILayout.Label(Application.dataPath);
+
+        }
+    }
+}
+
+
